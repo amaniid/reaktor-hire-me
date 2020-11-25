e2393636ae806b6f051046fa702cb9cefb6305aa
iida l <iidaliina.u@gmail.com>
2020-11-25 09:27:05 +0200

    wrote cover letter and fixed minor issues

--- app/main.py
+++ app/main.py
@@ -36,6 +36,9 @@ def package(pkgname):
     else:
         return render_template("404.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
